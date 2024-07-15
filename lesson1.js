ec112c7a64e06e696471fa644988cee59e14b7c6
richieroi <richieroigeek@gmail.com>
2024-07-15 22:35:00 +0100

    Create lesson1.js

---
+++ lesson1.js
@@ -0,0 +1,15 @@
+// Randomly generate a number between 1 and 10
+const randomNumber = Math.floor(Math.random() * 10) + 1;
+
+// Print the random number to the console
+console.log(randomNumber);
+
+// Create an array of random words
+const randomWords = ['apple', 'banana', 'carrot', 'dog', 'elephant'];
+
+// Select a random word from the array
+const randomIndex = Math.floor(Math.random() * randomWords.length);
+const randomWord = randomWords[randomIndex];
+
+// Print the random word to the console
+console.log(randomWord);
